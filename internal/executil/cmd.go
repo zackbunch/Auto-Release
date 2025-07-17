0d0306d3be62b8264fe1c58fdaa7cd49b73823eb
Zack Bunch <zackbunch96@gmail.com>
2025-07-17 11:03:46 -0700

    Added logic for executing commands

---
+++ internal/executil/cmd.go
@@ -0,0 +1,49 @@
+package executil
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// RunCMD executes the given command with inherited stdout/stderr
+func RunCMD(name string, args ...string) error {
+	return run("", false, name, args...)
+}
+
+// RunCMDWithDir executes the command in a specific directory
+func RunCMDWithDir(dir, name string, args ...string) error {
+	return run(dir, false, name, args...)
+}
+
+// DryRunCMD logs the command that would be run without executing
+func DryRunCMD(name string, args ...string) {
+	run("", true, name, args...)
+}
+
+// DryRunCMDWithDir logs the command that would be run in a specific directory
+func DryRunCMDWithDir(dir, name string, args ...string) {
+	run(dir, true, name, args...)
+}
+
+// internal runner to consolidate logic for stdout/stderr
+func run(dir string, dry bool, name string, args ...string) error {
+	fullCmd := fmt.Sprintf("%s %s", name, strings.Join(args, " "))
+	if dry {
+		fmt.Printf("[DRY RUN] %s\n", fullCmd)
+		return nil
+	}
+
+	cmd := exec.Command(name, args...)
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Printf("Running: %s\n", fullCmd)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("failed to run command '%s': %w", fullCmd, err)
+	}
+	return nil
+}
